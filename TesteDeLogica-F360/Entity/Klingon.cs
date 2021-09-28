7ce587a29e35f9f181bb3ed4a29e7b1f473ab66e
guics <guics@DESKTOP-GUILHER>
2021-09-28 19:57:17 -0300

    Add project files.

---
+++ TesteDeLogica-F360/Entity/Klingon.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteDeLogica_F360.Entity
+{
+    public class Klingon
+    {
+        public List<char> LetrasFoo { get; } = new List<char>() { 's', 'l', 'f', 'w', 'k' };
+
+        public List<char> AlfabetoKlingon { get; } = new List<char>("kbwrqdnfxjmlvhtcgzps".ToCharArray());
+
+        public List<char> Alfabeto { get; } = new List<char>("abcdefghijklmnopqrstuvwxyz".ToCharArray());
+
+        public List<string> Lista { get; set; }
+
+        public List<string> ListaVerbos { get; private set; }
+
+        public Klingon(string texto)
+        {
+            Lista = texto.Split(' ').ToList();
+            GeraListaVerbos();
+        }
+
+        public int CalculaPreposicao()
+        {
+            return Lista.Count(palavra => palavra.Length.Equals(3) &&
+                !palavra.Contains('d') &&
+                !LetrasFoo.Any(p => palavra.EndsWith(p.ToString())));
+        }
+        
+        public void GeraListaVerbos()
+        {
+            ListaVerbos = Lista.Where(palavra => palavra.Length >= 8 &&
+                LetrasFoo.Any(p => palavra.EndsWith(p.ToString())))
+                .ToList();
+        }
+
+        public int CalculaVerbosPrimeiraPessoa()
+        {
+            return ListaVerbos
+                .Where(verbo => !LetrasFoo.Any(p => verbo.StartsWith(p.ToString()))).Count();
+        }
+        
+        public string ListaVocabulario()
+        {      
+            return string.Join(" ", Lista.OrderBy(palavra => ConvertePalavra(palavra)).Distinct().ToList());
+        }
+
+        public string ConvertePalavra(string palavra) //Converte a palavra Klingon em no nosso alfabeto 
+        {
+            string retorno = "";
+            foreach (char letra in palavra.ToCharArray())
+                retorno += Alfabeto[AlfabetoKlingon.IndexOf(letra)];
+            
+            return retorno;
+        }
+
+        public int ContaNumerosBonitos()
+        {
+            return Lista.Select(palavra => ConverteParaNumero(palavra)).Count(n => n >= 440566 && n % 3 == 0);
+        }
+
+        public long ConverteParaNumero(string palavra)
+        {
+            long valor = 0;
+            for (int i = 0; i < palavra.Length; i++)
+                valor += Convert.ToInt64((AlfabetoKlingon.IndexOf(palavra[i]) * Math.Pow(20, i))); 
+
+            return valor;
+        }
+    }
+}
---
+++ TesteDeLogica-F360/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteDeLogica_F360.Entity;
+
+namespace TesteDeLogica_F360
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string escolhaMenu;
+            do
+            {
+                Console.Clear();
+                string escolhaTexto;
+                do
+                {
+                    Console.Write("Selecione o texto('A' ou 'B'): ");
+                    escolhaTexto = Console.ReadLine().ToUpper().Trim();
+                } while (!escolhaTexto.Equals("A") && !escolhaTexto.Equals("B"));
+
+                Console.Clear();
+                Console.WriteLine("Texto {0}: ", escolhaTexto);
+
+                var path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory())
+                    .Parent.FullName, "Textos/klingon-texto" + escolhaTexto + ".txt");
+
+                try
+                {
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        string texto = sr.ReadToEnd();
+                        if (!string.IsNullOrEmpty(texto))
+                        {
+                            var klingon = new Klingon(texto);
+                            Console.WriteLine("O texto possui " + klingon.CalculaPreposicao() + " preposições.");
+                            Console.WriteLine("O texto possui " + klingon.ListaVerbos.Count + " verbos, destes " +
+                                klingon.CalculaVerbosPrimeiraPessoa() + " estão em primeira pessoa.");
+                            Console.WriteLine("\n" + "O vocabulário ordenado é: \n" + klingon.ListaVocabulario());
+                            Console.WriteLine("\n" + "O texto possui {0} números bonitos", klingon.ContaNumerosBonitos());
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Erro: " + e.Message);
+                }
+                Console.Write("\n"+"Voltar a escolha?(S): ");
+                escolhaMenu = Console.ReadLine().ToUpper().Trim();
+            } while (escolhaMenu.Equals("S"));
+        }
+    }
+}
